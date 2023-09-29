6e4cf179ad5448ae021ac5507c425ad14639f89c
imadrml <102284276+imadrml@users.noreply.github.com>
2023-09-29 08:44:10 -0400

    Update 1ft_strrev.c

--- exam/ex02/1ft_strrev.c
+++ exam/ex02/1ft_strrev.c
@@ -25,6 +25,25 @@ char	*ft_strrev(char *str)
     }
     return (str);
 }
+#include <unistd.h>
+
+int ft_strlen(char *str);
+
+char *ft_strrev(char *str);
+
+int main(void) {
+    char myString[] = "Hello, World!";
+
+    // Reverse the string in-place
+    ft_strrev(myString);
+
+    // Print the reversed string
+    write(1, myString, ft_strlen(myString));
+    write(1, "\n", 1);
+
+    return 0;
+}
+
 /*
 Write a function that reverses (in-place) a string.
 
@@ -33,4 +52,8 @@ It must return its parameter.
 Your function must be declared as follows:
 
 char    *ft_strrev(char *str);
+The `ft_strrev` function reverses the string "Hello, World!" in-place. Here's the result of calling `ft_strrev(myString)`:
+
+Original string: "Hello, World!"
+Reversed string: "!dlroW ,olleH"
 */
